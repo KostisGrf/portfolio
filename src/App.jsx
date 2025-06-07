2964872bbad7ee5cb9a77d74a93a1b679ceab257
Kostis Grf <kostisgratsos@hotmail.gr>
2025-06-08 01:04:41 +0300

    Adds new package

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter,Route,Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
+import { Analytics } from "@vercel/analytics/react"
 
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
           <Route path="*"  element={<NotFound />}/>
         </Routes>
       </BrowserRouter>
+      <Analytics />
     </>
   )
 }
